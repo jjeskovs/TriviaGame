fb54704d003fa0ad3ffe281698cfeb51c2526118
Jegors Jeskovs <jegorsj@gmail.com>
2019-09-15 16:10:46 -0600

    timer and reset are working, all q's are in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,74 @@
 // List of global variables 
 var correctAnswer = 0;
 var inccorectAnswers = 0;
-var timer = 0;
+var time = 100;
+var questionNumber = 0;
+var intervalId;
 
 
 // Creating on click event that will start the game and display the first question
 
 $("#start").on("click", function(){
-
-    //$("#question").text(JSON.stringify(q1))
-    // to display one question on the screen
-    $("#question").text(q1.question)
-    $("#a").text(q1.a)
-    $("#b").text(q1.b)
-    $("#c").text(q1.c)
-    $("#d").text(q1.d)
+    
+    // This will display questions on the screen
+    $("#question").text(questions[questionNumber].question)
+    $("#a").text(questions[questionNumber].a)
+    $("#b").text(questions[questionNumber].b)
+    $("#c").text(questions[questionNumber].c)
+    $("#d").text(questions[questionNumber].d)
     //console.log(this)
     
+    // Timer starts
+    intervalId = setInterval(count, 1000);
     
+    // Score count
+    if (userInput === questions[questionNumber].correctAnswer) {
+        correctAnswer++;
+        //questionNumber++; // posibly be inside of reset function 
+        reset();
+    }
     
-})
+   
+       
+});
+
+function count() {
+    time--;
+    $("#timer").text(time);
+    if (time === 0){
+        reset();
+        
+    }
+}
+
+
+function reset() {
+    clearInterval(intervalId);
+    $("#question").text("");
+    $("#a").text("");
+    $("#b").text("");
+    $("#c").text("");
+    $("#d").text("");
+    questionNumber++
+
+}
 
 
 
 // Questions 
-var q1 = {
+var questions = [{
     "question": "One human hair can support how many kilograms?",
     "a": "Three",
     "b": "Five",
     "c": "Seven",
     "d": "Nine",
-}
+    "correctAnswer": "c"
+ }, 
+{
+    "question":"In ʻBen Hurʼ, which modern thing can be seen during the chario scene?",
+    "a": " A waitress",
+    "b": "A car",
+    "c": "A postbox",
+    "d": "A street lamp"
+
+}];
